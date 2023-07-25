cd820b09c41ce4b271f665f4bb4c0f54a59d892a
mariajazmin <lamasmariajazmin@gmail.com>
2023-07-25 03:36:10 -0300

    añadí el logo de mercado liebre

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require ("express");
 const app = express ();
 const path = require ("path");
 
+app.use(express.static("public"));
 app.listen(3000, ()=>"servidor escuchando en el puerto 3000!");
 
 app.get("/", (req, res)=>{
