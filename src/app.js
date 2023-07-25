a7949063d0425f604ab76b514a62a22ba15a12c7
mariajazmin <lamasmariajazmin@gmail.com>
2023-07-25 03:14:23 -0300

    trabajando en el home

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require ("express");
+const app = express ();
+const path = require ("path");
+
+app.listen(3000, ()=>"servidor escuchando en el puerto 3000!");
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/home.html"));
+});
